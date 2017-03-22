c346ffe11578a59ee8a3901e50574750a94ff595
olga-tsareko <olga.tsarenko7@gmail.com>
2017-03-23 00:03:19 +0200

    'changes-to-gallery'

--- wp-content/themes/bf-anna/gallery-page.php
+++ wp-content/themes/bf-anna/gallery-page.php
@@ -5,31 +5,25 @@
 
 get_header(); ?>
 
-    <section>
-        <div class="container">
-            <li class="album-grid"><a href="<?php the_permalink(); ?>"
-                                      title="<?php the_title(); ?>"><?php the_post_thumbnail('album-grid'); ?></a></li>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-            <?php if ($post->post_type == 'albums' && $post->post_status == 'publish') {
-                $attachments = get_posts(array(
-                    'post_type' => 'attachment',
-                    'posts_per_page' => -1,
-                    'post_parent' => $post->ID,
-                    'exclude' => get_post_thumbnail_id()
+            <?php
+            while ( have_posts() ) : the_post();
 
-                ));
-                if ($attachments) {
-                    foreach ($attachments as $attachment) {
-                        $class = "post-attachment mime-" . sanitize_title($attachment->post_mime_type);
-                        $title = wp_get_attachment_link($attachment->ID, 'album-grid', true);
-                        echo '<li class="' . $class . ' album-grid">' . $title . '</li>';
-                    }
-                }
-            }
+                get_template_part( 'template-parts/content', 'page' );
+
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+
+            endwhile; // End of the loop.
             ?>
-        </div>
-    </section>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
-
+get_sidebar();
 get_footer();
